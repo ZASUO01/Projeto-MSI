370018f7fbc89ed0cba80c818869ffdd36d7d3e9
pedro souza <pedro-souza@debian.0>
2026-04-15 18:41:31 -0300

    basic setup done

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,14 @@
 //
 // Created by pedro-souza on 15/04/2026.
 //
+#include "Game.h"
+
 int main() {
+    Game game;
+    if (game.Initialize()) {
+        game.RunLoop();
+    }
+    game.Shutdown();
+
     return 0;
 }
